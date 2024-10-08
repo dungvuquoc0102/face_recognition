302f3c80c5bf3a7993b4111e8b64c87e717ee392
dungvq <dung.vuquoc0102@gmail.com>
2024-10-08 23:30:19 +0700

    Add style

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const video = document.getElementById("video");
-const videoSection = document.querySelector(".video");
+(async () => {
+	await Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri("models"), faceapi.nets.faceLandmark68Net.loadFromUri("models"), faceapi.nets.faceRecognitionNet.loadFromUri("models"), faceapi.nets.faceExpressionNet.loadFromUri("models")]);
+	const video = document.getElementById("video");
+	navigator
+		.getUserMedia
+		// { video: {} },
+		// (stream) => (video.srcObject = stream),
+		// (err) => console.error(err)
+		();
+})();
 
-Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri("models"), faceapi.nets.faceLandmark68Net.loadFromUri("models"), faceapi.nets.faceRecognitionNet.loadFromUri("models"), faceapi.nets.faceExpressionNet.loadFromUri("models")]).then(startVideo);
-
-function startVideo() {
-	navigator.getUserMedia(
-		{ video: {} },
-		(stream) => (video.srcObject = stream),
-		(err) => console.error(err)
-	);
-}
 video.addEventListener("play", () => {
 	const canvas = faceapi.createCanvasFromMedia(video);
+	const videoSection = document.querySelector(".video");
 	videoSection.append(canvas);
 	const displaySize = { width: video.getBoundingClientRect().width, height: video.getBoundingClientRect().height };
 	faceapi.matchDimensions(canvas, displaySize);
@@ -34,7 +34,7 @@ video.addEventListener("play", () => {
 			detections.forEach((detection, i) => {
 				const qualityClass = detection.expressions.asSortedArray()[0].expression;
 				const percent = detection.expressions.asSortedArray()[0].probability.toFixed(0);
-				qualityLi.innerHTML = `Quality Class: ${qualityClass} (${percent * 100}%)`;
+				qualityLi.innerHTML = `Class Quality: ${qualityClass} (${percent * 100}%)`;
 			});
 		}
 	}, 1e3);
